2676d751cbfd756ac66c7da35d65d031379e1378
Ankur Phukon <ankurphukon44@gmail.com>
2020-11-03 12:57:43 +0530

    adding initial learning files done with the help of sentdex

---
+++ garage_light.py
@@ -0,0 +1,64 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setwarnings(False)
+GPIO.cleanup().
+GPIO.setmode(GPIO.BCM)
+
+TRIG = 4 
+ECHO = 18
+
+GREEN = 17
+YELLOW =27
+RED = 22
+
+GPIO.setup(TRIG, GPIO.OUT)
+GPIO.setup(ECHO, GPIO.IN)
+
+GPIO.setup(GREEN, GPIO.OUT)
+GPIO.setup(YELLOW, GPIO.OUT)
+GPIO.setup(RED, GPIO.OUT)
+
+def green_light():
+	GPIO.output(GREEN, GPIO.HIGH)
+	GPIO.output(YELLOW, GPIO.LOW)
+	GPIO.output(RED, GPIO.LOW)
+def yellow_light():
+	GPIO.output(GREEN, GPIO.LOW)
+	GPIO.output(YELLOW, GPIO.HIGH)
+	GPIO.output(RED, GPIO.LOW)
+
+def red_light():
+	GPIO.output(GREEN, GPIO.LOW)
+	GPIO.output(YELLOW, GPIO.LOW)
+	GPIO.output(RED, GPIO.HIGH)
+
+def get_distance():
+	GPIO.output(TRIG, True)
+	time.sleep(0.0001)
+	GPIO.output(TRIG, False)	
+	while GPIO.input(ECHO) ==  False:
+		start = time.time()
+	while GPIO.input(ECHO) ==  True:
+		end = time.time()
+	sig_time = end-start
+	#cm:
+	distance = sig_time/0.000058           	#inches: 0.000148
+	print('Distance:  {} cm' .format(distance)) 
+return distance
+
+while True: 
+	distance = get_distance()
+	time.sleep(0.05)
+	
+	if distance >= 9:
+		green_light()
+	elif distance > 6:
+		yellow_light()
+	elif distance <= 6:
+		red_light()
+
+
+
+
+
